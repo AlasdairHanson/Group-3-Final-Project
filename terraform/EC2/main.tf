73b4c8a844f97027a96e88ea8e879c978041a68b
AShakerQA <AShaker@qa.com>
2020-11-26 16:26:11 +0000

    AS: ec2 provisioning files added

---
+++ terraform/EC2/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "ec2"{
+  ami = var.ami_id
+  instance_type = var.instance
+  key_name = aws_key_pair.key.key_name
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_groups_ids
+  tags = {
+    name = var.name
+  }
+  associate_public_ip_address =var.enable_public_ip
+}
+
+resource "aws_key_pair" "key" {
+  key_name = "key"
+  public_key = "" //pass in a variable for security reasons, yet to determine
+
+
+}
+
+
+
+
+
+
+
---
+++ terraform/EC2/variables.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "ec2" {
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0dc8d444ee2a42d8a"
+}
+
+variable "subnet_id" {
+  description = "subnet id for EC2"
+}
+
+variable "name" {
+  description = "EC2 name"
+}
+
+variable "enable_public_ip" {
+  description = "enable public ip"
+  default     = true
+}
+
+variable "vpc_security_group_ids" {
+  description = "vpc security group ids"
+}
