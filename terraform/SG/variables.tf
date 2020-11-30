52101d0b591935934049b658ad7b270e4a448ac9
AShakerQA <AShaker@qa.com>
2020-11-30 15:03:30 +0000

    AS: configuring varaibles for sg

--- terraform/SG/variables.tf
+++ terraform/SG/variables.tf
@@ -1,4 +1,20 @@
+variable "web_sg_name" {
+  default = "default_web_sg"
+}
+
+variable "web_sg_description" {
+  default = "allow ssh connection"
+}
+
+variable "open_internet" {
+  default = "0.0.0.0/0"
+}
+
 variable "egress_ports" {
   type = list
   default = ["0.0.0.0/0"]
+}
+
+variable "vpc_id" {
+  description = "vpc id for sg"
 }
