4c8f7061768a57f0e89e24b83e9349d045aa01f0
AlanzoAlpha <meallydavis@yahoo.co.uk>
2020-12-01 16:37:49 +0000

    (A.D) Added username and password secret credentials

--- terraform/RDS/main.tf
+++ terraform/RDS/main.tf
@@ -14,9 +14,9 @@ resource "aws_db_instance" "testdb" {
   engine                 = "mysql"
   engine_version         = "5.7"
   instance_class         = "db.t2.micro"
-  name                   = "var.dname"
-  username               = "var.username"
-  password               = "var.pass"
+  name                   = "var.db_name"
+  username               = file(var.rds_username)
+  password               = file(var.rds_passwd)
   parameter_group_name   = "default.mysql5.7"
   skip_final_snapshot    = "true"
   db_subnet_group_name   =var.dbsubnet_gpname
--- terraform/RDS/variables.tf
+++ terraform/RDS/variables.tf
@@ -26,3 +26,17 @@ variable "subnet_ids" {
 variable "vpc_security_group_ids" {
   description = "list of sg ids"
 }
+
+variable "rds_password"{
+default = "~/credentials/rds_passwd"
+}
+
+variable "rds_username"{
+default = "~/credentials/rds_username"
+}
+
+variable "db_name"{
+default = "testdb"
+}
+
+
