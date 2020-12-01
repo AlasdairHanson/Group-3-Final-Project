93c0b2ebbfd3045afe0cf3dba021d8e45f642e2b
Basia Adamiec <badamiec3@live.com>
2020-12-01 14:57:37 +0000

    BA Unit testing continued

--- Spring-HelpQueue/src/main/java/com/qa/helpqueue/ticket/Ticket.java
+++ Spring-HelpQueue/src/main/java/com/qa/helpqueue/ticket/Ticket.java
@@ -2,96 +2,116 @@ package com.qa.helpqueue.ticket;
 
 public class Ticket {
 
-	//attributes
-		int ID;
-		String TicketTitle;
-		String TicketAuthor;
-		int TicketTime;
-		String TicketDesc;
-		String TicketUrgency;
-		String TicketTopic;
-		String TicketStatus;
-		String TicketTrainer;
-		String TicketCohort;
-		
+	// attributes
+	Long ID;
+	String TicketTitle;
+	String TicketAuthor;
+	int TicketTime;
+	String TicketDesc;
+	String TicketUrgency;
+	String TicketTopic;
+	String TicketStatus;
+	String TicketTrainer;
+	String TicketCohort;
+
+	public Ticket() {
+		super();
+	}
+
 	// Constructor
-		public Ticket(int id, String ticketTitle, String ticketAuthor, int ticketTime, String ticketDesc,
-				String ticketUrgency, String ticketTopic, String ticketStatus, String ticketTrainer, String ticketCohort) {
-			super();
-			ID = id;
-			TicketTitle = ticketTitle;
-			TicketAuthor = ticketAuthor;
-			TicketTime = ticketTime;
-			TicketDesc = ticketDesc;
-			TicketUrgency = ticketUrgency;
-			TicketTopic = ticketTopic;
-			TicketStatus = ticketStatus;
-			TicketTrainer = ticketTrainer;
-			TicketCohort = ticketCohort;
-		}
+	public Ticket(String ticketTitle, String ticketAuthor, int ticketTime, String ticketDesc, String ticketUrgency,
+			String ticketTopic, String ticketStatus, String ticketTrainer, String ticketCohort) {
+		super();
+		TicketTitle = ticketTitle;
+		TicketAuthor = ticketAuthor;
+		TicketTime = ticketTime;
+		TicketDesc = ticketDesc;
+		TicketUrgency = ticketUrgency;
+		TicketTopic = ticketTopic;
+		TicketStatus = ticketStatus;
+		TicketTrainer = ticketTrainer;
+		TicketCohort = ticketCohort;
+	}
+
 	// Getters and Setters
-		public int getID() {
-			return ID;
-		}
-		public void setID(int id) {
-			ID = id;
-		}
-		public String getTicketTitle() {
-			return TicketTitle;
-		}
-		public void setTicketTitle(String ticketTitle) {
-			TicketTitle = ticketTitle;
-		}
-		public String getTicketAuthor() {
-			return TicketAuthor;
-		}
-		public void setTicketAuthor(String ticketAuthor) {
-			TicketAuthor = ticketAuthor;
-		}
-		public String getTicketDesc() {
-			return TicketDesc;
-		}
-		public void setTicketDesc(String ticketDesc) {
-			TicketDesc = ticketDesc;
-		}
-		public String getTicketUrgency() {
-			return TicketUrgency;
-		}
-		public void setTicketUrgency(String ticketUrgency) {
-			TicketUrgency = ticketUrgency;
-		}
-		public String getTicketTopic() {
-			return TicketTopic;
-		}
-		public void setTicketTopic(String ticketTopic) {
-			TicketTopic = ticketTopic;
-		}
-		public String getTicketStatus() {
-			return TicketStatus;
-		}
-		public void setTicketStatus(String ticketStatus) {
-			TicketStatus = ticketStatus;
-		}
-		public String getTicketTrainer() {
-			return TicketTrainer;
-		}
-		public void setTicketTrainer(String ticketTrainer) {
-			TicketTrainer = ticketTrainer;
-		}
-		public String getTicketCohort() {
-			return TicketCohort;
-		}
-		public void setTicketCohort(String ticketCohort) {
-			TicketCohort = ticketCohort;
-		}
+	public Long getID() {
+		return ID;
+	}
 
-		public int getTicketTime() {
-			return TicketTime;
-		}
+	public void setID(Long id) {
+		ID = id;
+	}
 
-		public void setTicketTime(int ticketTime) {
-			TicketTime = ticketTime;
-		}
-		
+	public String getTicketTitle() {
+		return TicketTitle;
+	}
+
+	public void setTicketTitle(String ticketTitle) {
+		TicketTitle = ticketTitle;
+	}
+
+	public String getTicketAuthor() {
+		return TicketAuthor;
+	}
+
+	public void setTicketAuthor(String ticketAuthor) {
+		TicketAuthor = ticketAuthor;
+	}
+
+	public String getTicketDesc() {
+		return TicketDesc;
+	}
+
+	public void setTicketDesc(String ticketDesc) {
+		TicketDesc = ticketDesc;
+	}
+
+	public String getTicketUrgency() {
+		return TicketUrgency;
+	}
+
+	public void setTicketUrgency(String ticketUrgency) {
+		TicketUrgency = ticketUrgency;
+	}
+
+	public String getTicketTopic() {
+		return TicketTopic;
+	}
+
+	public void setTicketTopic(String ticketTopic) {
+		TicketTopic = ticketTopic;
+	}
+
+	public String getTicketStatus() {
+		return TicketStatus;
+	}
+
+	public void setTicketStatus(String ticketStatus) {
+		TicketStatus = ticketStatus;
+	}
+
+	public String getTicketTrainer() {
+		return TicketTrainer;
+	}
+
+	public void setTicketTrainer(String ticketTrainer) {
+		TicketTrainer = ticketTrainer;
+	}
+
+	public String getTicketCohort() {
+		return TicketCohort;
+	}
+
+	public void setTicketCohort(String ticketCohort) {
+		TicketCohort = ticketCohort;
+	}
+
+	public int getTicketTime() {
+		return TicketTime;
+	}
+
+	public void setTicketTime(int ticketTime) {
+		TicketTime = ticketTime;
+	}
+
 }
-
--- Spring-HelpQueue/src/test/java/com/qa/wishlist/service/TicketServiceUnitTest.java
+++ Spring-HelpQueue/src/test/java/com/qa/wishlist/service/TicketServiceUnitTest.java
@@ -1,5 +1,50 @@
 package com.qa.wishlist.service;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.qa.helpqueue.repo.HelpQueue_Repo;
+import com.qa.helpqueue.service.HelpQueue_Service;
+import com.qa.helpqueue.ticket.Ticket;
+
+@SpringBootTest
+@ActiveProfiles(profiles = "test")
 public class TicketServiceUnitTest {
+
+	@Autowired
+	private HelpQueue_Service service;
+
+	@MockBean
+	private HelpQueue_Repo repo;
+
+	@Test
+	void testCreateTicket() {
+		Long id = 1L;
+		Ticket newTicket = new Ticket("Title", "Author", 3, "Desc", "Urgency", "Topic", "Status", "Trainer", "Cohort");
+		Ticket savedTicket = new Ticket("Title", "Author", 3, "Desc", "Urgency", "Topic", "Status", "Trainer",
+				"Cohort");
+		savedTicket.setID(id);
+
+		Mockito.when(this.repo.save(newTicket)).thenReturn(savedTicket);
+	}
+
+	@Test
+	void testGetTicket() {
+
+	}
+
+	@Test
+	void testUpdateTicket() {
+
+	}
+
+	@Test
+	void testDeleteTicket() {
+
+	}
 
 }
