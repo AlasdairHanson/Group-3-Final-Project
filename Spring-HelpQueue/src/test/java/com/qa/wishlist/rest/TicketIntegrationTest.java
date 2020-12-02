76a88bc2a09e7d8873c06c4f376677c8aa0fbf59
Basia Adamiec <badamiec3@live.com>
2020-12-02 17:28:23 +0000

    BA finished integration testing

--- Spring-HelpQueue/src/test/java/com/qa/wishlist/rest/TicketIntegrationTest.java
+++ Spring-HelpQueue/src/test/java/com/qa/wishlist/rest/TicketIntegrationTest.java
@@ -1,5 +1,105 @@
 package com.qa.wishlist.rest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.helpqueue.ticket.Ticket;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles(profiles = "test")
+
 public class TicketIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMVC;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+	@Test
+	void testCreateTicket() throws Exception {
+		Ticket newTicket = new Ticket("Title", "Author", 3, "Desc", "Urgency", "Topic", "Status", "Trainer", "Cohort");
+		String requestBody = this.mapper.writeValueAsString(newTicket);
+
+		RequestBuilder request = post("/createTicket").contentType(MediaType.APPLICATION_JSON).content(requestBody);
+
+		ResultMatcher checkStatus = status().isCreated();
+
+		Ticket savedTicket = new Ticket("Title", "Author", 3, "Desc", "Urgency", "Topic", "Status", "Trainer",
+				"Cohort");
+		savedTicket.setID(2L);
+
+		String resultBody = this.mapper.writeValueAsString(savedTicket);
+		ResultMatcher checkBody = content().json(resultBody);
+
+		this.mockMVC.perform(request).andExpect(checkStatus).andExpect(checkBody);
+	}
+
+	@Test
+	void testRemoveTicket() throws Exception {
+
+		RequestBuilder request1 = delete("/removeTicket/1");
+		ResultMatcher checkStatus1 = status().isOk();
+
+		this.mockMVC.perform(request1).andExpect(checkStatus1);
+
+	}
+
+	@Test
+	void testReadTicket() throws Exception {
+		// MUST match the test database record
+		Ticket ticket = new Ticket("Some Title", "Some Author", 3, "Desc", "Urgent", "React", "Completed", "Savannah",
+				"Cloud Native");
+		ticket.setID(1L);
+		List<Ticket> tickets = new ArrayList<>();
+		tickets.add(ticket);
+		String responseBody = this.mapper.writeValueAsString(tickets);
+		System.out.println();
+		this.mockMVC.perform(get("/getTicket")).andExpect(status().isOk()).andExpect(content().json(responseBody));
+
+	}
+
+	@Test
+	void testUpdateTicket() throws Exception {
+		// Updates the book with id=1 in the test database
+		Long id = 1L;
+		Ticket updatedTicket = new Ticket("Some Updated Title", "Some Author", 5, "Desc", "Urgent", "React",
+				"Completed", "Savannah", "Cloud Native");
+		String requestBody = this.mapper.writeValueAsString(updatedTicket);
+		RequestBuilder request = put("/updateTicket/" + id).contentType(MediaType.APPLICATION_JSON)
+				.content(requestBody);
+
+		ResultMatcher checkStatus = status().isAccepted();
+
+		Ticket returnedTicket = new Ticket("Some Updated Title", "Some Author", 5, "Desc", "Urgent", "React",
+				"Completed", "Savannah", "Cloud Native");
+		returnedTicket.setID(id);
+
+		String resultBody = this.mapper.writeValueAsString(returnedTicket);
+		ResultMatcher checkBody = content().json(resultBody);
+
+		this.mockMVC.perform(request).andExpect(checkStatus).andExpect(checkBody);
+
+	}
 
 }
