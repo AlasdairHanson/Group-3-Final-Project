76cc286ca7bf4b3b0236599409409635023c237c
Basia Adamiec <badamiec3@live.com>
2020-12-04 15:18:04 +0000

    BA connection to backend established

--- frontend/fp-group3-ticket/src/Components/TicketList.jsx
+++ frontend/fp-group3-ticket/src/Components/TicketList.jsx
@@ -9,11 +9,12 @@ const TicketList = () => {
     const [ticketData, setTicketData] = useState([]);
 
     useEffect(() => {
-            axios.get("http://dummy.restapiexample.com/api/v1/employees")
+            axios.get("http://localhost:8081/getTicket")
                 .then((data) => {
                     setLoaded(true);
                     console.log(data);
-                    setTicketData(data.data.data);
+                    console.log(data.data);
+                    setTicketData(data.data);
                 }, (error) => {
                     setLoaded(true);
                     setError(error);
@@ -31,15 +32,15 @@ const TicketList = () => {
                     {ticketData.map((ticket) => (
                         <Ticket key={ticket.id} obj={ticket}
                         acc_id={ticket.id}
-                        title={ticket.employee_name}
-                        topic={ticket.employee_age}
-                        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        time={ticket.employee_age}
-                        status={ticket.employee_name}
-                        trainee={ticket.employee_name} 
-                        trainer={ticket.employee_name} 
-                        priority={ticket.employee_name}
-                        cohort={ticket.employee_name}/>
+                        title={ticket.ticketTitle}
+                        topic={ticket.ticketTopic}
+                        desc={ticket.ticketDesc}
+                        time={ticket.ticketTime}
+                        status={ticket.ticketStatus}
+                        trainee={ticket.ticketAuthor} 
+                        trainer={ticket.ticketTrainer} 
+                        priority={ticket.ticketPriority}
+                        cohort={ticket.ticketCohort}/>
                     ))}
                 </Accordion>
             </>
