76120937d2f60d120303302c1a25df0aee7fc213
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-12-06 18:34:16 +0000

    Update get-repo.sh

--- scripts/get-repo.sh
+++ scripts/get-repo.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
+rm -rf home/jenkins/.jenkins/workspace/mvnTesting/
+
+rm -rf /home/jenkins/.jenkins/workspace/mvnTesting@tmp/
+
 git clone https://github.com/AlasdairHanson/Group-3-Final-Project.git -b Testing
