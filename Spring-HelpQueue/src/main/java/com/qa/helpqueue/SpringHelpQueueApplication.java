303aa54bbb9e6ec49ea2dde1d6b3686d34bb8f48
AHanson2 <49337862+AHanson2@users.noreply.github.com>
2020-12-07 12:56:02 +0000

    AH: Updated app properties to target RDS

--- Spring-HelpQueue/src/main/java/com/qa/helpqueue/SpringHelpQueueApplication.java
+++ Spring-HelpQueue/src/main/java/com/qa/helpqueue/SpringHelpQueueApplication.java
@@ -3,6 +3,8 @@ package com.qa.helpqueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @EnableJpaRepositories
 @SpringBootApplication
