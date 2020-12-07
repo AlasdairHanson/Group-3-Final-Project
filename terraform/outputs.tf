0dd46e490e84253725ccba676aaf08c83de75fcf
AShakerQA <AShaker@qa.com>
2020-12-07 21:45:35 +0000

    AS: output the public_ip of vms

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "rds_endpoint_test" {
   value = module.test_rds.rds_endpoint_test
 }
+
+output "jenkinsvm_ip" {
+  value = module.Jenkinsvm.public_ip
+}
+
+output "testvm_ip" {
+  value = module.Testvm.public_ip
+}
