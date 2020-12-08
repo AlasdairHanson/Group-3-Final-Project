1f62930ea7ab82cbf0370a7e93aa77e2e08e6d33
Ubuntu <ubuntu@ip-172-31-45-130.eu-west-1.compute.internal>
2020-12-08 12:08:39 +0000

    (A.D) Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,31 +20,35 @@ fi
 
 #Start terraform
 
-cd ~/Group-3-Final-Project/terraform
-terraform init
-terraform plan
-terraform apply -auto-approve
+#cd ~/Group-3-Final-Project/terraform
+#terraform init
+#terraform plan
+#terraform apply -auto-approve
 
 #Export output ip addresses into variables to help mask secrets and prevent them from being pushed to git hub
 
-export jenkinsvm_ip="$(terraform output pulic_ip)"
-export testvm_ip="$(terraform output testvm)"
+#export jenkinsvm_ip="$(terraform output pulic_ip)"
+#export testvm_ip="$(terraform output testvm)"
+
+export jenkinsvm_ip=34.253.168.242
+export testvm_ip=34.246.187.233
+
 
 #Export output endpoints addresses into variables to help mask secrets and prevent them from being pushed to git hub
 
-export testdb_endpoint="$(terraform output rds_endpoint_test)"
-export db_endpoint="$(terraform output rds_endpoint_crud)"
+#export testdb_endpoint="$(terraform output rds_endpoint_test)"
+#export db_endpoint="$(terraform output rds_endpoint_crud)"
 
-cd ~/database
-mysql -h ${db_endpoint}.coaea37d1emt.eu-west-1.rds.amazonaws.com -P 3306 -u ${db_username} -p${passwd} < Create.sql
-mysql -h ${testdb_endpoint}.coaea37d1emt.eu-west-1.rds.amazonaws.com -P 3306 -u ${testdb_username} -p${passwd} < Create_test.sql
+#cd ~/database
+#mysql -h ${db_endpoint}.coaea37d1emt.eu-west-1.rds.amazonaws.com -P 3306 -u ${db_username} -p${passwd} < Create.sql
+#mysql -h ${testdb_endpoint}.coaea37d1emt.eu-west-1.rds.amazonaws.com -P 3306 -u ${testdb_username} -p${passwd} < Create_test.sql
 
 
 #Sleep 10 seconds to ensure these vm are fully up
 
 sleep 10
 
-#Make key directory if it does not already exist. This will store keys and allow ansible to exchanges these keys between vms to allow ssh to and from both jenkins vm and testvm.
+#Make key directory if it does not already exist. This will store keys that I secure copy from any vm and use them for shh'ing into any vm.
 
 cd ~
 rm -rf keys
@@ -86,7 +90,37 @@ EOF
 #Secure copy public key to the keys directory.
 
 scp ubuntu@${jenkinsvm_ip}:~/.ssh/id_rsa.pub ~/keys/jenkins
-    
+
+#Passing all vm keys into master's autherized_keys file and sharing that file with the other vms to allow ssh'ing from anywhere.
+
+cd ~
+cat ~/keys/jenkins/id_rsa.pub >> ~/.ssh/authorized_keys
+cat ~/keys/testvm/id_rsa.pub >> ~/.ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+scp ~/.ssh/authorized_keys ubuntu@${jenkinsvm_ip}:~/
+scp ~/.ssh/authorized_keys ubuntu@${testvm_ip}:~/
+
+ssh ubuntu@${testvm_ip} <<EOF
+cd ~
+cat ~/authorized_keys >> ~/.ssh/authorized_keys
+rm -rf authorized_keys
+
+EOF
+
+ssh ubuntu@${jenkinsvm_ip} <<EOF
+cd ~
+cat ~/authorized_keys >> ~/.ssh/authorized_keys
+rm -rf authorized_keys
+
+EOF
+
+#Passing in the hosts into the known hosts
+
+sudo echo "${jenkinsvm_ip} jenkinsvm_ip" >> /etc/hosts
+
+sudo echo "${testvm_ip} testvm_ip" >> /etc/hosts
+
+
 #Start anible playbook which installs all the neccessary softwares, add sudo doers and pass public keys into each vm to allow ssh'ing.
 
 cd ~/Group-3-Final-Project/ansible
@@ -97,4 +131,4 @@ ansible-playbook -i inventory playbook.yaml
     
 #sleep 5
 
-ssh -tt -i ~/.ssh/id_rsa ubuntu@${jenkinsvm_ip}
+#ssh -tt -i ~/.ssh/id_rsa ubuntu@${jenkinsvm_ip}
