57804b02d3de4912cc1a9838c42234c62a48ecd4
Basia Adamiec <badamiec3@live.com>
2020-12-11 11:40:06 +0000

    BA fixed Create.sql

--- DevOps/database/Create.sql
+++ DevOps/database/Create.sql
@@ -9,7 +9,7 @@ CREATE TABLE `ticket` (
     `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
     `ticket_title` VARCHAR(255) NOT NULL,
     `ticket_author` VARCHAR(255) NOT NULL,
-    `ticket_time` BIGINT NOT NULL,
+    `ticket_time` VARCHAR(255) NOT NULL,
     `ticket_desc` VARCHAR(255),
     `ticket_urgency` VARCHAR(255) NOT NULL,
     `ticket_topic` VARCHAR(255),
