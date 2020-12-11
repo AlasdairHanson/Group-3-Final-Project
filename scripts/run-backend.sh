6dec5874a8aed9f9db7c2d4c7874dfbcb122e70f
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-12-11 15:47:54 +0000

    Update run-backend.sh

--- scripts/run-backend.sh
+++ scripts/run-backend.sh
@@ -1,1 +1,21 @@
-bin bash
+#! /bin/bash
+
+cd /var/lib/jenkins
+
+. ./databasecredentials.sh
+
+echo ${testvm_ip}
+
+ssh ubuntu@${testvm_ip} <<EOF
+cd ~
+. ./databasecredentials.sh
+
+cd ~/Group-3-Final-Project/Spring-HelpQueue/target
+
+ls
+
+java -jar Spring-HelpQueue-0.0.1-SNAPSHOT.war
+
+curl localhost:8081/getTicket
+
+EOF
