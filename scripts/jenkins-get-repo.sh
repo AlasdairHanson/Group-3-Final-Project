0d7d1452dda784e1d79bb898c826095dcb037d16
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-12-11 14:58:01 +0000

    Update jenkins-get-repo.sh

--- scripts/jenkins-get-repo.sh
+++ scripts/jenkins-get-repo.sh
@@ -1,6 +1,6 @@
  #! /bin/bash
 cd /home/jenkins/.jenkins/workspace/project3
-if [ ! -d /home/jenkins/.jenkins/workspace/project3/Group-3-Final-Project ]; then
+if [ ! -d /var/lib/jenkins/workspace/project3/Group-3-Final-Project ]; then
    git clone https://github.com/AlasdairHanson/Group-3-Final-Project.git -b Dev
 else 
    rm -rf Group-3-Final-Project
