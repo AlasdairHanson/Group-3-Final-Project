daa7199d06012bb24c78b9a1929ea11edbb5b36b
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-12-11 15:09:24 +0000

    Update docker-build.sh

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -17,10 +17,10 @@ docker build -t ${DOCKER_USERNAME}/backend .
 
 echo 'Building frontend docker image'
 cd ~/Group-3-Final-Project/frontend/fp-group3-ticket
-docker build -t ${DOCKER_USERNAME}/frontend .
+sudo docker build -t ${DOCKER_USERNAME}/frontend .
 
 
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-docker push ${DOCKER_USERNAME}/frontend
-docker push ${DOCKER_USERNAME}/backend
+sudo docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+sudo docker push ${DOCKER_USERNAME}/frontend
+sudo docker push ${DOCKER_USERNAME}/backend
 EOF
