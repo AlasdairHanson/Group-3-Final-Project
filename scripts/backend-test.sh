1381ba0799fe3f0f8edbf3d8b9476529ff8383be
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-12-14 16:25:07 +0000

    Update backend-test.sh

--- scripts/backend-test.sh
+++ scripts/backend-test.sh
@@ -10,15 +10,11 @@ ssh ubuntu@${testvm_ip} <<EOF
 cd ~
 . ./databasecredentials.sh
 
-cp ~/.env ~/Group-3-Final-Project/Spring-HelpQueue/.env
-
-cp ~/.env ~/Group-3-Final-Project/.env
-
 cd ~/Group-3-Final-Project/Spring-HelpQueue
 
 which docker-compose
 
-docker-compose up -d
+sudo -E testdb_username=${testdb_username} docker-compose up -d
 
 docker exec backend bash -c "mvn clean package" | grep 'BUILD SUCCESS'
 
