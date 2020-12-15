2f600e257111f9c844d3cc40e9a6f67ec23597e0
Basia Adamiec <badamiec3@live.com>
2020-12-15 15:32:53 +0000

    BA fixed scripts

--- scripts/backend-test.sh
+++ scripts/backend-test.sh
@@ -31,13 +31,18 @@ echo ${DOCKER_PASSWORD}
 
 sudo docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 
+sudo docker stop $(docker ps -aq)
+sudo docker rm $(docker ps -aq)
+sudo docker rmi -f $(docker images -a -q)
+sudo docker system prune
+
 sudo docker build --build-arg testdb_endpoint="$testdb_endpoint" --build-arg db_username="$db_username" --build-arg testdb_username="$testdb_username" --build-arg db_endpoint="$db_endpoint" --build-arg password="$password" -t ${DOCKER_USERNAME}/backend:latest /home/ubuntu/Group-3-Final-Project/Spring-HelpQueue/
 
 sudo docker push ${DOCKER_USERNAME}/backend:latest
 
 sudo docker stop $(docker ps -aq)
 sudo docker rm $(docker ps -aq)
-docker rmi -f $(docker images -a -q)
+sudo docker rmi -f $(docker images -a -q)
 sudo docker system prune
 
 EOF
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -13,7 +13,13 @@ cd ~
 
 echo 'Building frontend docker image'
 cd ~/Group-3-Final-Project/frontend/fp-group3-ticket
-docker build -t ${DOCKER_USERNAME}/frontend:latest .
+
+sudo docker stop $(docker ps -aq)
+sudo docker rm $(docker ps -aq)
+sudo docker rmi -f $(docker images -a -q)
+sudo docker system prune
+
+sudo docker build -t ${DOCKER_USERNAME}/frontend:latest .
 
 echo ${DOCKER_USERNAME}
 echo ${DOCKER_PASSWORD}
@@ -24,7 +30,7 @@ sudo docker push ${DOCKER_USERNAME}/frontend:latest
 
 sudo docker stop $(docker ps -aq)
 sudo docker rm $(docker ps -aq)
-docker rmi -f $(docker images -a -q)
+sudo docker rmi -f $(docker images -a -q)
 sudo docker system prune
 
 EOF
