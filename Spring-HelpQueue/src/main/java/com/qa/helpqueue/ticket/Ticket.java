d909acc99bea17374d712e30a198448de51fe914
AlasdairHanson <AlasdairHanson@gmail.com>
2020-12-15 10:54:14 +0000

    AH: Changed ticket attribute names to be shorter

--- Spring-HelpQueue/src/main/java/com/qa/helpqueue/ticket/Ticket.java
+++ Spring-HelpQueue/src/main/java/com/qa/helpqueue/ticket/Ticket.java
@@ -14,15 +14,15 @@ public class Ticket {
     //attributes
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long ID;
-        private String TicketTitle;
-        private String TicketAuthor;
-        private String TicketTime;
-        private String TicketDesc;
+        private Long id;
+        private String title;
+        private String author;
+        private String timestamp;
+        private String description;
         private String priority;
         private String topic;
         private String status;
-        private String TicketTrainer;
+        private String trainer;
         private String cohort;
         
         public Ticket() {
@@ -33,14 +33,14 @@ public class Ticket {
         public Ticket(String ticketTitle, String ticketAuthor, String ticketTime, String ticketDesc,
                 String ticketUrgency, String ticketTopic, String ticketStatus, String ticketTrainer, String ticketCohort) {
             super();
-            this.TicketTitle = ticketTitle;
-            this.TicketAuthor = ticketAuthor;
-            this.TicketTime = ticketTime;
-            this.TicketDesc = ticketDesc;
+            this.title = ticketTitle;
+            this.author = ticketAuthor;
+            this.timestamp = ticketTime;
+            this.description = ticketDesc;
             this.priority = ticketUrgency;
             this.topic = ticketTopic;
             this.status = ticketStatus;
-            this.TicketTrainer = ticketTrainer;
+            this.trainer = ticketTrainer;
             this.cohort = ticketCohort;
         }
     // Getters and Setters
@@ -48,61 +48,61 @@ public class Ticket {
  
 
         public Long getID() {
-            return ID;
+            return id;
         }
 
  
 
         public void setID(Long iD) {
-            ID = iD;
+            id = iD;
         }
 
  
 
         public String getTicketTitle() {
-            return TicketTitle;
+            return title;
         }
 
  
 
         public void setTicketTitle(String ticketTitle) {
-            TicketTitle = ticketTitle;
+            title = ticketTitle;
         }
 
  
 
         public String getTicketAuthor() {
-            return TicketAuthor;
+            return author;
         }
 
  
 
         public void setTicketAuthor(String ticketAuthor) {
-            TicketAuthor = ticketAuthor;
+            author = ticketAuthor;
         }
 
  
 
         public String getTicketTime() {
-            return TicketTime;
+            return timestamp;
         }
 
  
 
         public void setTicketTime(String ticketTime) {
-            TicketTime = ticketTime;
+            timestamp = ticketTime;
         }
 
  
 
         public String getTicketDesc() {
-            return TicketDesc;
+            return description;
         }
 
  
 
         public void setTicketDesc(String ticketDesc) {
-            TicketDesc = ticketDesc;
+            description = ticketDesc;
         }
 
  
@@ -144,13 +144,13 @@ public class Ticket {
  
 
         public String getTicketTrainer() {
-            return TicketTrainer;
+            return trainer;
         }
 
  
 
         public void setTicketTrainer(String ticketTrainer) {
-            TicketTrainer = ticketTrainer;
+            trainer = ticketTrainer;
         }
 
  
@@ -169,24 +169,24 @@ public class Ticket {
 
         @Override
         public String toString() {
-            return "Ticket [ID=" + ID + ", TicketTitle=" + TicketTitle + ", TicketAuthor=" + TicketAuthor
-                    + ", TicketTime=" + TicketTime + ", TicketDesc=" + TicketDesc + ", TicketUrgency=" + priority
+            return "Ticket [ID=" + id + ", TicketTitle=" + title + ", TicketAuthor=" + author
+                    + ", TicketTime=" + timestamp + ", TicketDesc=" + description + ", TicketUrgency=" + priority
                     + ", TicketTopic=" + topic + ", TicketStatus=" + status + ", TicketTrainer="
-                    + TicketTrainer + ", TicketCohort=" + cohort + "]";
+                    + trainer + ", TicketCohort=" + cohort + "]";
         }
 
 		@Override
 		public int hashCode() {
 			final int prime = 31;
 			int result = 1;
-			result = prime * result + ((TicketAuthor == null) ? 0 : TicketAuthor.hashCode());
+			result = prime * result + ((author == null) ? 0 : author.hashCode());
 			result = prime * result + ((cohort == null) ? 0 : cohort.hashCode());
-			result = prime * result + ((TicketDesc == null) ? 0 : TicketDesc.hashCode());
+			result = prime * result + ((description == null) ? 0 : description.hashCode());
 			result = prime * result + ((status == null) ? 0 : status.hashCode());
-			result = prime * result + ((TicketTime == null) ? 0 : TicketTime.hashCode());
-			result = prime * result + ((TicketTitle == null) ? 0 : TicketTitle.hashCode());
+			result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
+			result = prime * result + ((title == null) ? 0 : title.hashCode());
 			result = prime * result + ((topic == null) ? 0 : topic.hashCode());
-			result = prime * result + ((TicketTrainer == null) ? 0 : TicketTrainer.hashCode());
+			result = prime * result + ((trainer == null) ? 0 : trainer.hashCode());
 			result = prime * result + ((priority == null) ? 0 : priority.hashCode());
 			return result;
 		}
@@ -200,45 +200,45 @@ public class Ticket {
 			if (getClass() != obj.getClass())
 				return false;
 			Ticket other = (Ticket) obj;
-			if (TicketAuthor == null) {
-				if (other.TicketAuthor != null)
+			if (author == null) {
+				if (other.author != null)
 					return false;
-			} else if (!TicketAuthor.equals(other.TicketAuthor))
+			} else if (!author.equals(other.author))
 				return false;
 			if (cohort == null) {
 				if (other.cohort != null)
 					return false;
 			} else if (!cohort.equals(other.cohort))
 				return false;
-			if (TicketDesc == null) {
-				if (other.TicketDesc != null)
+			if (description == null) {
+				if (other.description != null)
 					return false;
-			} else if (!TicketDesc.equals(other.TicketDesc))
+			} else if (!description.equals(other.description))
 				return false;
 			if (status == null) {
 				if (other.status != null)
 					return false;
 			} else if (!status.equals(other.status))
 				return false;
-			if (TicketTime == null) {
-				if (other.TicketTime != null)
+			if (timestamp == null) {
+				if (other.timestamp != null)
 					return false;
-			} else if (!TicketTime.equals(other.TicketTime))
+			} else if (!timestamp.equals(other.timestamp))
 				return false;
-			if (TicketTitle == null) {
-				if (other.TicketTitle != null)
+			if (title == null) {
+				if (other.title != null)
 					return false;
-			} else if (!TicketTitle.equals(other.TicketTitle))
+			} else if (!title.equals(other.title))
 				return false;
 			if (topic == null) {
 				if (other.topic != null)
 					return false;
 			} else if (!topic.equals(other.topic))
 				return false;
-			if (TicketTrainer == null) {
-				if (other.TicketTrainer != null)
+			if (trainer == null) {
+				if (other.trainer != null)
 					return false;
-			} else if (!TicketTrainer.equals(other.TicketTrainer))
+			} else if (!trainer.equals(other.trainer))
 				return false;
 			if (priority == null) {
 				if (other.priority != null)
--- Spring-HelpQueue/src/main/resources/schema.sql
+++ Spring-HelpQueue/src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE ticket (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    ticket_title VARCHAR(255),
-    ticket_author VARCHAR(255),
-    ticket_time DATETIME,
-    ticket_desc VARCHAR(255),
+    title VARCHAR(255),
+    author VARCHAR(255),
+    timestamp DATETIME,
+    description VARCHAR(255),
     priority VARCHAR(255),
     topic VARCHAR(255),
     status VARCHAR(255),
-    ticket_trainer VARCHAR(255),
+    trainer VARCHAR(255),
     cohort VARCHAR(255)
 
 );
