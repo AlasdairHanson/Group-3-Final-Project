49cb3b396bb02f3b0697762c47c0732e9b3299f8
Reece-elder <71394624+Reece-elder@users.noreply.github.com>
2020-12-15 21:38:59 +0000

    Create data.sql

---
+++ Spring-HelpQueue/src/main/resources/data.sql
@@ -0,0 +1,68 @@
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (1, 'curabitur gravida nisi at nibh in hac habitasse platea dictumst aliquam augue quam', 'Tricia Corner', '2020-10-30 21:29:48', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.
+
+Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
+
+Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.', 'High', 'stable', 'Unresolved', 'Koralle Proger', 'DevOps');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (2, 'cras pellentesque volutpat dui maecenas tristique est et tempus semper', 'Kim Gooderham', '2020-08-08 19:55:27', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', 'Low', 'user-facing', 'Unresolved', 'Dalenna Cottam', 'DevOps');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (3, 'id justo sit amet sapien dignissim vestibulum', 'Dasie Mitcheson', '2020-03-30 07:17:32', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+
+Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', 'High', 'Monitored', 'Resolved', 'Enrica Toye', 'CloudNative');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (4, 'turpis enim blandit mi in porttitor pede justo eu massa donec dapibus duis at velit', 'Larissa Sherborn', '2019-12-21 00:48:46', 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.
+
+Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 'Medium', 'forecast', 'Unresolved', 'Ludvig Hemphrey', 'CloudNative');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (5, 'hendrerit at vulputate vitae nisl aenean lectus pellentesque', 'Gavra Eccott', '2019-12-23 06:46:03', 'In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 'High', 'actuating', 'Ongoing', 'Babb Mold', 'CloudNative');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (6, 'condimentum curabitur in libero ut massa volutpat', 'Gaynor Rearie', '2020-07-25 13:04:58', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.
+
+Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 'Medium', 'access', 'Ongoing', 'Melamie Fairham', 'CloudNative');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (7, 'nullam porttitor lacus at turpis donec posuere metus vitae ipsum', 'Francyne Tassell', '2020-04-09 11:29:18', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', 'High', 'Self-enabling', 'Resolved', 'Kelcy Croll', 'Other');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (8, 'proin eu mi nulla ac enim in tempor turpis', 'Nicolai Yorke', '2020-09-04 14:20:59', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 'Medium', 'hub', 'Ongoing', 'Denyse Hannen', 'Testing');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (9, 'ut ultrices vel augue vestibulum ante ipsum primis', 'Michel Huxton', '2020-01-07 07:06:05', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', 'High', 'Organic', 'Resolved', 'Robinson Senn', 'BigData');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (10, 'primis in faucibus orci luctus et ultrices posuere cubilia curae nulla', 'Fredelia Janman', '2020-06-11 08:24:04', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
+
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
+
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', 'Medium', 'interface', 'Resolved', 'Bertrand Attew', 'BigData');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (11, 'ac lobortis vel dapibus at diam nam', 'Catlee Tuffs', '2020-05-08 17:52:23', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+
+Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', 'Medium', '24 hour', 'Unresolved', 'Nydia Moutray Read', 'Other');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (12, 'ligula in lacus curabitur at ipsum ac tellus semper interdum mauris ullamcorper purus sit', 'Giordano Lemerle', '2020-09-01 11:20:30', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.', 'High', 'Cross-group', 'Ongoing', 'Roch Flaws', 'DevOps');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (13, 'augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis', 'Sheelah Gladyer', '2020-07-16 09:42:30', 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
+
+In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 'Critical', 'workforce', 'Resolved', 'Meara Lardner', 'Other');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (14, 'neque sapien placerat ante nulla justo aliquam', 'Brooks Semeniuk', '2020-04-30 07:09:06', 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.
+
+Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 'High', 'web-enabled', 'Resolved', 'Nester Patnelli', 'Software');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (15, 'lobortis ligula sit amet eleifend pede libero quis orci nullam molestie', 'Arlin Paulusch', '2020-05-06 07:48:15', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
+
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
+
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', 'High', 'well-modulated', 'Ongoing', 'Stormy Dobbings', 'CloudNative');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (16, 'sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel', 'Meade Beardshall', '2020-04-09 13:19:58', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
+
+Phasellus in felis. Donec semper sapien a libero. Nam dui.
+
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 'Medium', 'Fully-configurable', 'Ongoing', 'Gerrard Parley', 'Other');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (17, 'porta volutpat erat quisque erat eros viverra eget', 'Joline Yves', '2020-04-20 08:39:12', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'High', 'installation', 'Resolved', 'Beck Fillon', 'CloudNative');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (18, 'risus praesent lectus vestibulum quam sapien', 'Wandie Salvador', '2020-09-24 00:06:15', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'High', 'Public-key', 'Resolved', 'Jo Wittier', 'CloudNative');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (19, 'a odio in hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla', 'Sheeree McGilvray', '2020-02-13 23:11:30', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.', 'Medium', 'Enterprise-wide', 'Ongoing', 'Westleigh Aggott', 'DevOps');
+insert into MOCK_DATA (id, title, author, timestamp, content, priority, topic, status, trainer, cohort) values (20, 'pede libero quis orci nullam molestie', 'Dorie Orlton', '2020-02-28 07:56:44', 'Fusce consequat. Nulla nisl. Nunc nisl.', 'High', 'Balanced', 'Resolved', 'Ainslee Downing', 'DevOps');
