5db72c2f4e1f7ded88977c3ab6256de350048081
Basia Adamiec <badamiec3@live.com>
2020-12-15 15:40:18 +0000

    BA altered scripts

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -14,10 +14,8 @@ cd ~
 echo 'Building frontend docker image'
 cd ~/Group-3-Final-Project/frontend/fp-group3-ticket
 
-sudo docker stop $(docker ps -aq)
-sudo docker rm $(docker ps -aq)
-sudo docker rmi -f $(docker images -a -q)
-sudo docker system prune
+sudo docker rmi $(docker images -q)
+sudo docker system prune --volumes -a -f 
 
 sudo docker build -t ${DOCKER_USERNAME}/frontend:latest .
 
@@ -28,9 +26,7 @@ echo ${DOCKER_PASSWORD}
 sudo docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 sudo docker push ${DOCKER_USERNAME}/frontend:latest
 
-sudo docker stop $(docker ps -aq)
-sudo docker rm $(docker ps -aq)
-sudo docker rmi -f $(docker images -a -q)
-sudo docker system prune
+sudo docker rmi $(docker images -q)
+sudo docker system prune --volumes -a -f 
 
 EOF
--- scripts/docker-cleanup.sh
+++ scripts/docker-cleanup.sh
@@ -2,16 +2,6 @@
 
 cd ~/Group-3-Final-Project/frontend/fp-group3-ticket
 
-sudo docker stop $(docker ps -aq)
-sudo docker rm $(docker ps -aq)
-sudo docker rmi -f $(docker images -a -q)
-sudo docker system prune
+pwd
 
-cd ~/Group-3-Final-Project/Spring-HelpQueue
-
-sudo docker stop $(docker ps -aq)
-sudo docker rm $(docker ps -aq)
-sudo docker rmi -f $(docker images -a -q)
-sudo docker system prune
-
-echo "Docker cleanup complete"
+echo "Docker cleanup skipped"
