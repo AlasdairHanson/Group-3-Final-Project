756d2cffe1b76075ff32b4509cfbcd5625ae5e81
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-12-15 10:47:53 +0000

    Update backend-test.sh

--- scripts/backend-test.sh
+++ scripts/backend-test.sh
@@ -33,9 +33,6 @@ sudo docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 
 sudo docker build --build-arg testdb_endpoint="$testdb_endpoint" --build-arg db_username="$db_username" --build-arg testdb_username="$testdb_username" --build-arg db_endpoint="$db_endpoint" --build-arg password="$password" -t ${DOCKER_USERNAME}/backend:latest /home/ubuntu/Group-3-Final-Project/Spring-HelpQueue/
 
-cp ~/.env ~/Group-3-Final-Project/Spring-HelpQueue/.env
-
-sudo docker run --env-file=.env -d -p 8081:8081 --name backend ${DOCKER_USERNAME}/backend:latest
 
 EOF
                     
