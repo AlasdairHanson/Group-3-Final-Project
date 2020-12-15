d111caac2098d5be08492fb57dbe729185ce0843
AlanzoAlpha <67590124+AlanzoAlpha@users.noreply.github.com>
2020-12-15 16:52:53 +0000

    Update kube.sh
    (A.D) Update

--- scripts/kube.sh
+++ scripts/kube.sh
@@ -3,6 +3,7 @@
 cd /var/lib/jenkins
 . ./databasecredentials.sh
 
+echo ${testvm_ip}
 
 cd /var/lib/jenkins/workspace/project3/Group-3-Final-Project/DevOps/kubernetes
 sudo kubectl apply -f ~/secrets.yaml
