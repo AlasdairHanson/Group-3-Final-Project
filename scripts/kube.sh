751f4592fcffe14e534e933584a2dd3ba69b9de0
AlanzoAlpha <67590124+AlanzoAlpha@users.noreply.github.com>
2020-12-15 19:20:51 +0000

    Update kube.sh

--- scripts/kube.sh
+++ scripts/kube.sh
@@ -15,13 +15,13 @@ echo ${testvm_ip}
 
 cd /var/lib/jenkins/workspace/project3/Group-3-Final-Project/DevOps/kubernetes
 
-#sudo kubectl create namespace group3
+sudo kubectl create namespace group3
 sudo kubectl delete pods --all pods --namespace=group3
 sudo kubectl apply -f nginx.yaml 
 sudo kubectl apply -f nginx-conf.yaml
 sudo kubectl apply -f frontend.yaml
 sudo kubectl apply -f backend.yaml
 sudo kubectl apply -f nginx-lb.yaml
-#kubectl describe service nginx-lb --namespace=group3
-kubectl describe service nginx-lb
+kubectl describe service nginx-lb --namespace=group3
+#kubectl describe service nginx-lb
 echo "Done"
