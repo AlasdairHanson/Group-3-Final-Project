407080ed334fc5cc054fdb8c4d8e23bc0a12b168
AShakerQA <71396007+AShakerQA@users.noreply.github.com>
2020-12-16 11:09:11 +0000

    added secrets and rearranged creation order

--- scripts/kube.sh
+++ scripts/kube.sh
@@ -20,12 +20,14 @@ echo ${testdb_username}
 
 cd /var/lib/jenkins/workspace/project3/Group-3-Final-Project/DevOps/kubernetes
 
+sudo kubectl apply -f secrets.yaml
 sudo kubectl create namespace group3
 sudo kubectl delete pods --all pods --namespace=group3
-sudo kubectl apply -f nginx.yaml 
-sudo kubectl apply -f nginx-conf.yaml
+sudo kubectl apply -f secrets.yaml
 sudo kubectl apply -f frontend.yaml
 sudo kubectl apply -f backend.yaml
+sudo kubectl apply -f nginx.yaml 
+sudo kubectl apply -f nginx-conf.yaml
 sudo kubectl apply -f nginx-lb.yaml
 sudo kubectl describe service nginx-lb
 echo "Done"
