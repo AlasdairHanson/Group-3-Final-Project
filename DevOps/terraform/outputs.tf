a5b91f96a223677fef012be16deb912664e71855
AlanzoAlpha <meallydavis@yahoo.co.uk>
2020-12-17 11:06:19 +0000

    (A.D) Corrected terraform outputs

--- DevOps/terraform/outputs.tf
+++ DevOps/terraform/outputs.tf
@@ -10,10 +10,6 @@ output "testvm_ip" {
   value = module.Testvm.public_ip
 }
 
-#output "BastionHost_ip" {
-#  value = module.BastionHost.public_ip
-#}
-
 output "rds_endpoint_prod" {
   value = module.prod_rds.rds_endpoint
 }
