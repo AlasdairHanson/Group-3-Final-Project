5bd62276e09dd7f8d25764902ce0196185b6c91b
AShakerQA <71396007+AShakerQA@users.noreply.github.com>
2020-12-18 09:19:11 +0000

    AS: Updated Kube.sh

--- scripts/kube.sh
+++ scripts/kube.sh
@@ -26,11 +26,8 @@ sudo kubectl delete -f secrets.yaml
 sudo kubectl delete -f nginx.yaml
 sudo kubectl delete -f nginx-lb.yaml
 sudo kubectl delete -f nginx-conf.yaml
-#sudo kubectl delete -f fe-nginx-conf.yaml
-#sudo kubectl delete -f fe-nginx.yaml
 sudo kubectl delete -f backend.yaml
 sudo kubectl delete -f frontend.yaml
-#sudo kubectl delete -f fe-nginx-lb.yaml
 
 sudo kubectl create namespace group3
 sudo kubectl delete pods --all pods --namespace=group3
@@ -40,9 +37,6 @@ sudo kubectl apply -f backend.yaml
 sudo kubectl apply -f nginx-conf.yaml
 sudo kubectl apply -f nginx.yaml 
 sudo kubectl apply -f nginx-lb.yaml
-#sudo kubectl apply -f fe-nginx-conf.yaml
-#sudo kubectl apply -f fe-nginx.yaml
-#sudo kubectl apply -f fe-nginx-lb.yaml
 
 sleep 20
 
